e56a777402de1557388b30ab3111da4aeaab147a
BobFarreras <143645540+BobFarreras@users.noreply.github.com>
2025-09-09 22:23:54 +0200

    experimental

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    // AFEGEIX O MODIFICA AQUEST BLOC
+    experimental: {
+      typedRoutes: false,
+    },
 };
 
 export default nextConfig;
